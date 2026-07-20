b3a46f29f8c7558245517abe3c82e8de9d6f9812
DarioRodriguez09 <darioroel22@gmail.com>
2026-07-20 16:50:06 -0600

    Paquete Documentos
    Se creo la clase de documento

--- src/editor_textos/Documento.java
+++ src/editor_textos/Documento.java
@@ -9,5 +9,44 @@ package editor_textos;
  * @author Dario R
  */
 public class Documento {
+    //Atributos de Documento
+    private String nombre;
+    private String ruta;
+    private String contenido;
+    private boolean guardado;
     
+    //Constructor
+
+    public Documento(String nombre, String ruta, String contenido, boolean guardado) {
+        this.nombre = nombre;
+        this.ruta = ruta;
+        this.contenido = contenido;
+        this.guardado = guardado;
+    }
+    //Metodos getter
+    public String getNombre() {
+        return nombre;
+    }
+    public String getRuta() {
+        return ruta;
+    }
+    public String getContenido() {
+        return contenido;
+    }
+    public boolean isGuardado() {
+        return guardado;
+    }
+    //Metodos setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+    public void setGuardado(boolean guardado) {
+        this.guardado = guardado;
+    }
 }
