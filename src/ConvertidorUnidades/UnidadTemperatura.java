0e916c9b1116a46c520943cd7e7f0a3fed8ba662
Daniel <danielariassolano@gmail.com>
2026-07-20 16:42:23 -0600

    se agrego dos clases
    se agrego una clase enum y otra clase con sus funciones

---
+++ src/ConvertidorUnidades/UnidadTemperatura.java
@@ -0,0 +1,17 @@
+package ConvertidorUnidades;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public enum UnidadTemperatura {
+    CELSIUS,
+    FAHRENHEIT,
+    KELVIN,
+    RANKINE
+}
