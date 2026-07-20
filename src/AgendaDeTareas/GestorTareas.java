7e3151f47b5c4d5828972e2c90ed96aee1d73f22
JaxanderBC <barahonajack3@gmail.com>
2026-07-20 14:10:19 -0600

    Se creo la clase Agenta de Tareas
    Se agrego clase tarea, estado, gestortareas

---
+++ src/AgendaDeTareas/GestorTareas.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AgendaDeTareas;
+
+/**
+ *
+ * @author USER
+ */
+public class GestorTareas {
+    //Atributos
+    private Tarea[] tareas;
+    private int CantidadTareas;
+    
+    //M get
+    public int getCantidadTareas() {
+        return CantidadTareas;
+    }
+    
+    //M Constructor
+    public GestorTareas(Tarea[] tareas) {
+        this.tareas = new Tarea[100];
+    }
+    
+    //Funciones
+    public void agregarTarea(Tarea tarea) {
+        if (CantidadTareas < tareas.length) {
+            tareas[CantidadTareas] = tarea;
+            CantidadTareas++;
+        }
+    }
+    public void eliminarTarea(int index) {
+        for (int i = index; i < CantidadTareas - 1; i++) {
+            tareas[i] = tareas[i + 1];
+        }
+        tareas[CantidadTareas - 1] = null;
+        CantidadTareas--;
+    }
+    public Tarea obtenerTarea(int index) {
+        return tareas[index];
+    }
+}
---
+++ src/AgendaDeTareas/Tarea.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AgendaDeTareas;
+
+/**
+ *
+ * @author USER
+ */
+public class Tarea {
+    //Atributos
+    private String Descripcion;
+    private EstadoTarea Estado;
+    
+    //M get
+    public String getDescripcion() {
+        return Descripcion;
+    }
+    public EstadoTarea getEstado() {
+        return Estado;
+    }
+    
+    //M set
+    public void setEstado(EstadoTarea Estado) {
+        this.Estado = Estado;
+    }
+     public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+     
+    //M constructor
+    public Tarea(String Descripcion) {
+        this.Descripcion = Descripcion;
+        this.Estado = EstadoTarea.PENDIENTE;
+    }
+
+    //To String
+    @Override
+    public String toString() {
+        String texto = (this.Estado == EstadoTarea.PENDIENTE) ? "Pendiente" : "Completada";
+        return "Descripcion:" + this.Descripcion + "\nEstado:"+ texto;
+    }
+    
+}
